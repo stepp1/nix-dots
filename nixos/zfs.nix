482090ba8d2576360009791fb59bda24bc88f29a
Stepp1 <sfaragg@gmail.com>
2022-12-14 14:48:17 -0300

    [flake] cleaning old files

--- nixos/zfs.nix
+++
@@ -1,5 +0,0 @@
-{ config, pkgs, ... }: {
-  # check https://openzfs.github.io/openzfs-docs/Getting%20Started/NixOS/index.html
-  boot.supportedFilesystems = [ "zfs" ];
-  networking.hostId = "5df9bf34";
-}
--- scripts/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-pushd ~/nix-dots
-sudo nixos-rebuild switch --flake .#
-popd

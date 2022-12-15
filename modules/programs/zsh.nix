1087c6db815c348bae014d4ddbe2f796bcd0e7fd
Stepp1 <sfaragg@gmail.com>
2022-12-14 21:59:17 -0300

    [zsh] correct alias for hey

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -7,14 +7,18 @@
 
     shellAliases = {
       ll = "ls -l";
+
       update = "~/nix-dots/bin/update.sh";
       update-user = "~/nix-dots/bin/update-user.sh";
       update-system = "~/nix-dots/bin/update-system.sh";
       update-versions = "~/nix-dots/bin/update-versions.sh";
       hey = "~/nix-dots/bin/hey";
+
       generations = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
+
       vi = "nvim";
       vim = "nvim";
+
       dots = "cd ~/nix-dots";
     };
 

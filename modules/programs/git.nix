6b9456657105da43684f866539989c67c24521e5
Stepp1 <sfaragg@gmail.com>
2022-12-15 21:15:22 -0300

    [hm] git conf

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,19 +1,13 @@
 { pkgs, lib, config, ... }:
 
-with lib;
-let cfg = config.modules.git;
-
-in {
-  options.modules.git = { enable = mkEnableOption "git"; };
-  config = mkIf cfg.enable {
-    programs.git = {
-      enable = true;
-      userName = "Stepp1";
-      userEmail = "sfaragg@gmail.com";
-      extraConfig = {
-        pull.rebase = false;
-        init = { defaultBranch = "master"; };
-      };
+{
+  programs.git = {
+    enable = true;
+    userName = "Stepp1";
+    userEmail = "sfaragg@gmail.com";
+    extraConfig = {
+      pull.rebase = false;
+      init = { defaultBranch = "master"; };
     };
   };
 }

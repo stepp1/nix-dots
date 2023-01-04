2b77536f98f3d6346ff9ebb1b79c5aa9a635c41a
Stepp1 <sfaragg@gmail.com>
2023-01-04 02:51:52 -0300

    [gnome] xorg

--- hosts/gnome.nix
+++ hosts/gnome.nix
@@ -4,7 +4,7 @@
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
-    displayManager.gdm.wayland = true;
+    displayManager.gdm.wayland = false;
     desktopManager.gnome.enable = true;
   };
 
